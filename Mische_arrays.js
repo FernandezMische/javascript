49ab933a8ae18669b06beca33a706d6a0f367361
FernandezMische <cheelizafernandez999@gmail.com>
2025-10-30 08:42:20 +0200

    JS basics first

---
+++ Mische_arrays.js
@@ -0,0 +1,16 @@
+// TODO: Create an empty array called colors
+let colors =[]
+// TODO: Add three colors to the array using push()
+colors.push("blue","red","green");
+console.log(colors)
+// TODO: Create another array called numbers with five numbers
+let numbers =[1,2,3,4,5]
+// TODO: Remove the last color from the colors array
+colors.splice(2,1);
+console.log(colors)
+
+// TODO: Add a new color to the beginning of the colors array
+colors.unshift("yellow");
+console.log(colors)
+// TODO: Print the length of both arrays
+console.log(numbers.length,colors.length)
---
+++ Mische_basics.js
@@ -0,0 +1,12 @@
+// TODO: Create a variable called firstName and store your first name in it
+const FirstName="Mische"
+// TODO: Create a variable called lastName and store your last name in it
+const LastName="Fernandez"
+// TODO: Create a variable called age using let (you'll modify this later)
+let age =26
+// TODO: Create a fullName variable that combines firstName and lastName
+const FullName =FirstName+" "+LastName
+// TODO: Print a greeting to the console using these variables
+console.log(FirstName,LastName,age,FullName);
+// TODO: Try changing the age variable to a different value
+ age =25
