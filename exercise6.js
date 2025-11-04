f1972d7474367c959afefbf496656a86c84f00f9
FernandezMische <cheelizafernandez999@gmail.com>
2025-11-04 11:03:29 +0200

    first functions

---
+++ exercise6.js
@@ -0,0 +1,18 @@
+// TODO: Create a function called greet that takes a name parameter 
+// TODO: Make the greet function return "Hello, [name]!" 
+function greet (Name) {
+return `Hello, ${Name}`
+}
+console.log (greet("Che"));
+
+// TODO: Create a function called add that takes two parameters 
+// TODO: Make the add function return the sum of both parameters 
+function add (a,b){
+    return a+b
+}
+console.log (add(2,2))
+// TODO: Call both functions and store their results in variables 
+let Greeting = greet("Che")
+let Sum = add(2,2)
+// TODO: Print the results to the console 
+console.log(Greeting,Sum);
